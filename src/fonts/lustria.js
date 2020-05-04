0f1b5f2923d617dbfdd2c602d953fe0b588133ae
larrygf02 <raulgf_02@hotmail.com>
2020-05-03 23:34:05 -0500

    React add fonts Lustria

---
+++ src/fonts/lustria.js
@@ -0,0 +1,9 @@
+import LustriaFont from './Lustria/Lustria-Regular.ttf'
+export const lustria = {
+    fontFamily: 'Lustria',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    src: `
+        url(${LustriaFont}) format('ttf')
+    `,
+}
