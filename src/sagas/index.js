3365aae9f4f5fad5c01f0255269311d2825697bf
larrygf02 <raulgf_02@hotmail.com>
2020-05-09 14:02:02 -0500

    Draft component render complete

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,8 +1,10 @@
 import stories from "./storie";
+import drafts from "./draft"
 import { all } from "redux-saga/effects";
 
 export default function* rootSaga() {
     yield all([
-        stories()
+        stories(),
+        drafts()
     ]);
 }
