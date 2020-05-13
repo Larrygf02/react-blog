a0b082989bfbfad505b6e62ef7deccac3ffea21a
larrygf02 <raulgf_02@hotmail.com>
2020-05-12 22:49:40 -0500

    Redirect to login

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import { lustria } from './fonts/lustria';
 import { unstable_createMuiStrictModeTheme as createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import MenuBar from './components/Menu';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import MyStories from './components/stories/PageYourStories';
 import { Provider } from 'react-redux';
 import store from './store';
+import SignIn from './components/login/SignIn';
 
 //import MenuBar from './components/MenuMaterial';
 
@@ -23,7 +24,7 @@ const theme = createMuiTheme({
     },
   },
 });
-
+let isAuthenticated = false
 function App() {
   return (
     <BrowserRouter>
@@ -33,7 +34,10 @@ function App() {
           <div>
             <MenuBar></MenuBar>
             <Switch>
-              <Route exact path="/stories" component={MyStories}></Route>
+              <Route exact path="/login" component={SignIn}></Route>
+              <Route exact path="/stories" render={() => (
+                isAuthenticated === true ? <MyStories/> : <Redirect to="/login"/>
+              )}></Route>
             </Switch>
           </div>
         </ThemeProvider>
---
+++ src/components/login/SignIn.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+function SignIn() {
+    return (
+        <Fragment>
+            SignIn
+        </Fragment>
+    )
+}
+
+export default SignIn;
