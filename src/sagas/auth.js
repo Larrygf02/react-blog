a90766dcd04d3b4f7747b8a329d037d6e527240b
larrygf02 <raulgf_02@hotmail.com>
2020-05-12 23:57:01 -0500

    Auth saga

---
+++ src/sagas/auth.js
@@ -0,0 +1,15 @@
+import { START_LOGIN, SUCCESS_LOGIN, FAILED_LOGIN } from "../actions/authAction";
+import { takeLatest, put } from "redux-saga/effects";
+
+function* startLogin(obj) {
+    console.log(obj);
+    try {
+        yield put({ type: SUCCESS_LOGIN })
+    } catch (error) {
+        yield put({ type: FAILED_LOGIN })
+    }
+}
+// watchers
+export default function* auth() {
+    yield takeLatest(START_LOGIN, startLogin)
+}
