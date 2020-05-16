f82afe25de68a490adba79626ec5495603de273c
larrygf02 <raulgf_02@hotmail.com>
2020-05-15 20:40:37 -0500

    Feat: Actions new user

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -1,6 +1,9 @@
 export const START_LOGIN = 'START_LOGIN'
 export const SUCCESS_LOGIN = 'SUCCESS_LOGIN'
 export const FAILED_LOGIN = 'FAILED_LOGIN'
+export const START_NEW_USER = 'START_NEW_USER'
+export const SUCCESS_NEW_USER = 'SUCCESS_NEW_USER'
+export const FAILED_NEW_USER = 'FAILED_NEW_USER'
 
 export const startLogin = payload => ({
     type: 'START_LOGIN',
@@ -14,5 +17,20 @@ export const successLogin = payload => ({
 
 export const failedLogin = payload => ({
     type: 'FAILED_LOGIN',
+    ...payload
+})
+
+export const startNewUser = payload => ({
+    type: 'START_NEW_USER',
+    ...payload
+})
+
+export const successNewUser = payload => ({
+    type: 'SUCCESS_NEW_USER',
+    ...payload
+})
+
+export const failedNewUser = payload => ({
+    type: 'FAILED_NEW_USER',
     ...payload
 })
