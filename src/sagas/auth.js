1240c24e746ae267867d33f66502566044134025
larrygf02 <raulgf_02@hotmail.com>
2020-05-15 20:51:40 -0500

    Saga newUser

--- src/sagas/auth.js
+++ src/sagas/auth.js
@@ -1,6 +1,7 @@
-import { START_LOGIN, SUCCESS_LOGIN, FAILED_LOGIN } from "../actions/authAction";
+import { START_LOGIN, SUCCESS_LOGIN, FAILED_LOGIN, START_NEW_USER, SUCCESS_NEW_USER, FAILED_NEW_USER } from "../actions/authAction";
 import { takeLatest, put } from "redux-saga/effects";
 import axios from "axios";
+
 const headers = {
     'Content-Type': 'application/json',
 }
@@ -19,7 +20,21 @@ function* startLogin(payload) {
         yield put({ type: FAILED_LOGIN })
     }
 }
+
+function* newUser(payload) {
+    console.log(payload);
+    const data = yield axios.post('http://localhost:5000/user', payload)
+                            .then(response => response)
+                            .catch(error => console.log(error))
+    console.log(data);
+    try {
+        yield put({ type: SUCCESS_NEW_USER })
+    } catch (error) {
+        yield put({ type: FAILED_NEW_USER })
+    }
+}
 // watchers
 export default function* auth() {
     yield takeLatest(START_LOGIN, startLogin)
+    yield takeLatest(START_NEW_USER, newUser)
 }
