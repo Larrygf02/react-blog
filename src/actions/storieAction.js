a524c1a27e111d5dc1e65acaf86cc4e90a0cc364
larrygf02 <raulgf_02@hotmail.com>
2020-05-19 22:50:53 -0500

    Get all stories

--- src/actions/storieAction.js
+++ src/actions/storieAction.js
@@ -1,5 +1,8 @@
 export const START_GET_STORIES = 'START_GET_STORIES'
 export const SUCCESS_GET_STORIES = 'SUCCESS_GET_STORIES'
+export const START_GET_ALL_STORIES = 'START_ALL_GET_STORIES'
+export const SUCCESS_GET_ALL_STORIES = 'SUCCESS_GET_ALL_STORIES'
+export const FAILED_GET_ALL_STORIES = 'FAILED_GET_ALL_STORIES'
 
 export const startGetStories = payload => ({
     type: 'START_GET_STORIES',
@@ -8,6 +11,21 @@ export const startGetStories = payload => ({
 
 export const successGetStories = payload => ({
     type: 'SUCCESS_GET_STORIES',
+    ...payload
+})
+
+export const startGetAllStories = payload => ({
+    type: 'START_GET_ALL_STORIES',
+    ...payload
+})
+
+export const successGetAllStories = payload => ({
+    type: 'SUCCESS_GET_ALL_STORIES',
+    ...payload
+})
+
+export const failedGetAllStories = payload => ({
+    type: 'FAILED_GET_ALL_STORIES',
     ...payload
 })
 
--- src/sagas/storie.js
+++ src/sagas/storie.js
@@ -1,10 +1,10 @@
-import { put, takeLatest } from 'redux-saga/effects'
-import { START_GET_STORIES, SUCCESS_GET_STORIES } from '../actions/storieAction'; 
+import { put, takeLatest, takeEvery } from 'redux-saga/effects'
+import { START_GET_STORIES, SUCCESS_GET_STORIES, START_GET_ALL_STORIES, SUCCESS_GET_ALL_STORIES, FAILED_GET_ALL_STORIES } from '../actions/storieAction'; 
 import axios from 'axios';
+import clientAxios from '../config/axios';
 
 function* getStories({ payload }) {
     try {
-        console.log('La funcion esta escuchando')
         const stories = yield axios.get('http://localhost:5000/storie/user/3')
             .then(response => response.data)
             .catch(error => console.log(error))
@@ -16,7 +16,21 @@ function* getStories({ payload }) {
     }
 }
 
+
+function* getAllStories({ payload }) {
+    try {
+        const stories = yield clientAxios.get('/stories')
+                                        .then(response => response.data)
+                                        .catch(error => console.log(error))
+        console.log(stories);
+        yield put({ type: SUCCESS_GET_ALL_STORIES, payload: stories})
+    } catch (error) {
+        yield put({ type: FAILED_GET_ALL_STORIES })
+    }
+}
 // watchers
+
 export default function* stories() {
     yield takeLatest(START_GET_STORIES, getStories);
+    yield takeEvery(START_GET_ALL_STORIES, getAllStories);
 }
