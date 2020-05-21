29d899db4b949b97ca13868bcd8cb001022757e1
larrygf02 <raulgf_02@hotmail.com>
2020-05-21 00:09:06 -0500

    SaveNewStorie sagas

--- src/sagas/storie.js
+++ src/sagas/storie.js
@@ -1,8 +1,7 @@
 import { put, takeLatest } from 'redux-saga/effects'
-import { START_GET_STORIES, SUCCESS_GET_STORIES, START_GET_ALL_STORIES, SUCCESS_GET_ALL_STORIES, FAILED_GET_ALL_STORIES } from '../actions/storieAction'; 
+import { START_GET_STORIES, SUCCESS_GET_STORIES, START_GET_ALL_STORIES, SUCCESS_GET_ALL_STORIES, FAILED_GET_ALL_STORIES, SUCCESS_NEW_STORIE, START_NEW_STORIE, FAILED_NEW_STORIE } from '../actions/storieAction'; 
 import axios from 'axios';
 //import clientAxios from '../config/axios';
-
 function* getStories({ payload }) {
     console.log('Entro stories');
     try {
@@ -29,8 +28,20 @@ function* getAllStories() {
         yield put({ type: FAILED_GET_ALL_STORIES })
     }
 }
+
+function * saveNewStorie(action) {
+    const { history, payload } = action;
+    try {
+        yield axios.post('http://localhost:5000/storie', payload)
+        yield put({ type: SUCCESS_NEW_STORIE })
+        history.push("/")
+    } catch (error) {
+        yield put({ type: FAILED_NEW_STORIE })
+    }
+}
 // watchers
 export default function* stories() {
     yield takeLatest(START_GET_STORIES, getStories)
     yield takeLatest(START_GET_ALL_STORIES, getAllStories)
+    yield takeLatest(START_NEW_STORIE, saveNewStorie)
 }
