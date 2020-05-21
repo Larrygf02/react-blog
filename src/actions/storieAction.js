7e084119add7e3218d33053b2c7b60c53c082e83
larrygf02 <raulgf_02@hotmail.com>
2020-05-20 23:58:36 -0500

    New Storie Action

--- src/actions/storieAction.js
+++ src/actions/storieAction.js
@@ -3,6 +3,10 @@ export const SUCCESS_GET_STORIES = 'SUCCESS_GET_STORIES'
 export const START_GET_ALL_STORIES = 'START_GET_ALL_STORIES'
 export const SUCCESS_GET_ALL_STORIES = 'SUCCESS_GET_ALL_STORIES'
 export const FAILED_GET_ALL_STORIES = 'FAILED_GET_ALL_STORIES'
+// NEW STORIE
+export const START_NEW_STORIE = 'START_NEW_STORIE'
+export const SUCCESS_NEW_STORIE = 'SUCCESS_NEW_STORIE'
+export const FAILED_NEW_STORIE = 'FAILED_NEW_STORIE'
 
 export const startGetStories = payload => ({
     type: 'START_GET_STORIES',
@@ -28,4 +32,21 @@ export const failedGetAllStories = payload => ({
     type: 'FAILED_GET_ALL_STORIES',
     ...payload
 })
+
+export const startNewStorie = payload => ({
+    type: 'START_NEW_STORIE',
+    ...payload
+})
+
+export const successNewStorie = payload => ({
+    type: 'SUCCESS_NEW_STORIE',
+    ...payload
+})
+
+export const failedNewStorie = payload => ({
+    type: 'FAILED_NEW_STORIE',
+    ...payload
+})
+
+
 
