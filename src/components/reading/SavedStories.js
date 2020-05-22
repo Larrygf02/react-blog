cfd28a60bd8c4418d2ae19ccbd0b1585f7b70a51
larrygf02 <raulgf_02@hotmail.com>
2020-05-21 22:18:11 -0500

    No hay datos en historias guardadas

--- src/components/reading/SavedStories.js
+++ src/components/reading/SavedStories.js
@@ -2,8 +2,11 @@ import React, { Fragment } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { startGetSavedStories } from '../../actions/readAction';
 import { useEffect } from 'react';
+import { Divider } from '@material-ui/core';
+import useStylesGlobal from '../utils/StylesGlobal';
 
 function SavedStories() {
+    const classes = useStylesGlobal();
     const dispatch = useDispatch();
     // acceder al state
     const user = useSelector(state => state.auth.user)
@@ -13,9 +16,21 @@ function SavedStories() {
         dispatch(startGetSavedStories({userid}))
     }, [dispatch, userid])
 
+    const loading = useSelector(state => state.reading.loading)
+    const saved_stories = useSelector(state => state.reading.saved_stories)
+    
     return (
         <Fragment>
-            <h3>Historias Guardadas</h3>
+            { loading ? 'Cargando ...': null}
+            { saved_stories.length === 0 ? 'No hay datos': null }
+            { saved_stories.map(storie => (
+                <div key={storie.ID}>
+                    <h2>{storie.Title}</h2>
+                    <p className={classes.grey}>{storie.Content}</p>
+                    <span className={classes.grey}>{classes.CreatedAt}</span>
+                    <Divider/>
+                </div>
+            ))}
         </Fragment>
     )
 }
