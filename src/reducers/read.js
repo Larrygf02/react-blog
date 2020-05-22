b55ef54c6b82f1bb27eea2495213dbf21ddfbaf6
larrygf02 <raulgf_02@hotmail.com>
2020-05-21 22:41:35 -0500

    Reducer in archived storeis

--- src/reducers/read.js
+++ src/reducers/read.js
@@ -1,6 +1,7 @@
 import { START_GET_SAVED_STORIES, SUCCESS_GET_SAVED_STORIES, FAILED_GET_SAVED_STORIES, START_GET_ARCHIVED_STORIES, SUCCESS_GET_ARCHIVED_STORIES, FAILED_GET_ARCHIVED_STORIES } from '../actions/readAction';
 const initialState = {
     saved_stories: [],
+    archived_stories: [],
     error: null,
     loading: false
 }
@@ -32,7 +33,7 @@ export default function(state=initialState, action) {
         case SUCCESS_GET_ARCHIVED_STORIES:
             return {
                 ...state,
-                saved_stories: action.payload,
+                archived_stories: action.payload,
                 loading: false
             }
         case FAILED_GET_ARCHIVED_STORIES:
