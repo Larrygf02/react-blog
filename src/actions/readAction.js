c75fa123c8b37a7137fdb35b7dbf06158f2483c5
larrygf02 <raulgf_02@hotmail.com>
2020-05-21 22:31:38 -0500

    Read Action archiveds

--- src/actions/readAction.js
+++ src/actions/readAction.js
@@ -1,6 +1,10 @@
 export const START_GET_SAVED_STORIES = 'START_GET_SAVED_STORIES';
 export const SUCCESS_GET_SAVED_STORIES = 'SUCCESS_GET_SAVED_STORIES'
 export const FAILED_GET_SAVED_STORIES = 'FAILED_GET_SAVED_STORIES'
+// Archived
+export const START_GET_ARCHIVED_STORIES = 'START_GET_ARCHIVED_STORIES';
+export const SUCCESS_GET_ARCHIVED_STORIES = 'SUCCESS_GET_ARCHIVED_STORIES';
+export const FAILED_GET_ARCHIVED_STORIES = 'FAILED_GET_ARCHIVED_STORIES'
 
 export const startGetSavedStories = payload => ({
     type: 'START_GET_SAVED_STORIES',
@@ -14,5 +18,20 @@ export const successGetSavedStories = payload => ({
 
 export const failedGetSavedStories = payload => ({
     type: 'FAILED_GET_SAVED_STORIES',
+    ...payload
+})
+
+export const startGetArchivedStories = payload => ({
+    type: 'START_GET_ARCHIVED_STORIES',
+    ...payload
+})
+
+export const successGetArchivedStories = payload => ({
+    type: 'SUCCESS_GET_ARCHIVED_STORIES',
+    ...payload
+})
+
+export const failedGetArchivedStories = payload => ({
+    type: 'FAILED_GET_ARCHIVED_STORIES',
     ...payload
 })
