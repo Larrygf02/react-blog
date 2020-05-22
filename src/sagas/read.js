e763ea4cd4d264dcee2affad4800ef77f04ca9c3
larrygf02 <raulgf_02@hotmail.com>
2020-05-21 22:36:52 -0500

    Archived stories sagas

--- src/sagas/read.js
+++ src/sagas/read.js
@@ -1,6 +1,6 @@
 import { put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
-import { START_GET_SAVED_STORIES, SUCCESS_GET_SAVED_STORIES, FAILED_GET_SAVED_STORIES } from "../actions/readAction";
+import { START_GET_SAVED_STORIES, SUCCESS_GET_SAVED_STORIES, FAILED_GET_SAVED_STORIES, START_GET_ARCHIVED_STORIES, SUCCESS_GET_ARCHIVED_STORIES, FAILED_GET_ARCHIVED_STORIES } from "../actions/readAction";
 
 function* getSavedStories(payload) {
     const { userid } = payload;
@@ -12,7 +12,18 @@ function* getSavedStories(payload) {
     }
 }
 
+function* getArchivedStories(payload) {
+    const { userid } = payload;
+    try {
+        const { data } = yield axios.get(`http://localhost:5000/user/storie/archiveds/${userid}`)
+        yield put({ type: SUCCESS_GET_SAVED_STORIES, payload: data })
+    } catch (error) {
+        yield put({ type: FAILED_GET_ARCHIVED_STORIES })
+    }
+}
+
 // watchers
 export default function* read() {
     yield takeLatest(START_GET_SAVED_STORIES, getSavedStories)
+    yield takeLatest(START_GET_ARCHIVED_STORIES, getArchivedStories)
 }
