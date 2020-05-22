46d86d55f399af6335669175bda99c9cdb97bff0
larrygf02 <raulgf_02@hotmail.com>
2020-05-21 22:46:01 -0500

    Render components

--- src/components/reading/ArchivedStories.js
+++ src/components/reading/ArchivedStories.js
@@ -1,9 +1,34 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Divider } from '@material-ui/core';
+import useStylesGlobal from '../utils/StylesGlobal';
+import { startGetArchivedStories } from '../../actions/readAction';
 
 function ArchivedStories() {
+    const classes = useStylesGlobal();
+    const dispatch = useDispatch();
+    // acceder al state
+    const user = useSelector(state => state.auth.user)
+    const { ID:userid } = user;
+    useEffect(() => {
+        console.log(userid);
+        dispatch(startGetArchivedStories({userid}))
+    }, [dispatch, userid])
+
+    const loading = useSelector(state => state.reading.loading)
+    const archived_stories = useSelector(state => state.reading.archived_stories)
     return (
         <Fragment>
-            <h3>Historias Archivadas</h3>
+            { loading ? 'Cargando...': null}
+            { archived_stories.length === 0 ? 'No hay datos': null }
+            { archived_stories.map(storie => (
+                <div key={storie.ID}>
+                    <h2>{storie.Title}</h2>
+                    <p className={classes.grey}>{storie.Content}</p>
+                    <span className={classes.grey}>{classes.CreatedAt}</span>
+                    <Divider/>
+                </div>
+            ))}
         </Fragment>
     )
 }
