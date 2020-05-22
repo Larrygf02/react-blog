965e500c743cf79c2cbed45b44abfc70402fc7d2
larrygf02 <raulgf_02@hotmail.com>
2020-05-21 22:40:36 -0500

    Archived histories reducers

--- src/reducers/read.js
+++ src/reducers/read.js
@@ -1,5 +1,4 @@
-import { START_GET_SAVED_STORIES, SUCCESS_GET_SAVED_STORIES, FAILED_GET_SAVED_STORIES } from '../actions/readAction';
-
+import { START_GET_SAVED_STORIES, SUCCESS_GET_SAVED_STORIES, FAILED_GET_SAVED_STORIES, START_GET_ARCHIVED_STORIES, SUCCESS_GET_ARCHIVED_STORIES, FAILED_GET_ARCHIVED_STORIES } from '../actions/readAction';
 const initialState = {
     saved_stories: [],
     error: null,
@@ -20,6 +19,23 @@ export default function(state=initialState, action) {
                 loading: false
             }
         case FAILED_GET_SAVED_STORIES:
+            return {
+                ...state,
+                loading: false,
+                error: true
+            }
+        case START_GET_ARCHIVED_STORIES:
+            return {
+                ...state,
+                loading: true
+            }
+        case SUCCESS_GET_ARCHIVED_STORIES:
+            return {
+                ...state,
+                saved_stories: action.payload,
+                loading: false
+            }
+        case FAILED_GET_ARCHIVED_STORIES:
             return {
                 ...state,
                 loading: false,
